ff310f79eccaebbce3d57f40e87936deefca5e23
ahirugumi <puts.helloworld@nifty.com>
2012-07-11 01:24:32 +0900

    Continuation of datetime.

--- datetime.php
+++ datetime.php
@@ -1,4 +1,13 @@
 <?php
 print "strftime()：" . strftime('%c') . "\n";
 print "date()：" . date('r') . "\n";
+
+//よくある日本でのフォーマット
+print "strftime() YYYY/MM/DD HH:MI:SS：" . strftime('%Y/%m/%d %H:%M:%S') . "\n";
+print "date() YYYY/MM/DD HH:MI:SS：" . date('Y/m/d H:i:s') . "\n";
+
+//タイムスタンプ
+$timestamp=mktime(14, 12, 13, 05, 20, 1992);
+print $timestamp . "\n";
+print strftime('%Y/%m/%d %H:%M:%S', $timestamp) . "\n";
 ?>
